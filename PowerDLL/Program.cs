20c2f9d30bb495dbd362f6d32534a0642f9d1350
Filip Ceglik <fceglik@gmail.com>
2021-12-28 14:51:36 +0100

    Upload

---
+++ PowerDLL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace PowerDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var script = string.Join(" ", args);
+            var results = Shell.RunScript(script);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ PowerDLL/Shell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+
+namespace PowerDLL
+{
+    internal class Shell
+    {
+        public static ICollection<PSObject> RunScript(string command)
+        {
+            using (var runspace = RunspaceFactory.CreateRunspace())
+            {
+                runspace.Open();
+                using (var pipeline = runspace.CreatePipeline())
+                {
+                    pipeline.Commands.AddScript(command);
+                    var results = pipeline.Invoke();
+                    
+                    return results;
+                }
+            }
+        }
+
+    }
+}
